c144c9d47facb2412301effcc92a508eaecc3eef
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-08-13 20:39:55 -0300

    adicionando roda pé

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,10 @@ export default function Home() {
       <h2>Exemplo h2</h2>
       <h3>exemplo h3</h3>
       <p>Texto parágrafo</p>
+      <Link href='/' >Home</Link>
+      <Link href='/usuarios/login' >Login</Link>
+      <Link href='/usuarios/cadastro' >Cadastro</Link>
+      <Link href='/sobre' >Sobre</Link>
     </div>
   );
 }
